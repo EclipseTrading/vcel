df0a02c014769c5b96bcb15fad19fb65bdd16ed6
Will Liu <wliu@eclipseoptions.com>
2020-10-09 11:21:35 +0800

    VC-3208 support dictionary access in VCEL

---
+++ src/VCEL.Core/DictionaryAccessor.cs
@@ -0,0 +1,18 @@
+﻿namespace VCEL.Core
+{
+    public class DictionaryAccessor<T> : IValueAccessor<T>
+    {
+        private string propName;
+
+        public DictionaryAccessor(string propName)
+        {
+            this.propName = propName;
+        }
+
+        public T GetValue(IContext<T> o)
+        {
+            var dictContext = (DictionaryContext<T>)o;
+            return o.Monad.Lift(dictContext.Dict[propName]);
+        }
+    }
+}
---
+++ src/VCEL.Core/DictionaryContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using VCEL.Monad;
+
+namespace VCEL.Core
+{
+    public class DictionaryContext<T> : IContext<T>
+    {
+        public DictionaryContext(IMonad<T> monad, IReadOnlyDictionary<string, object> dict)
+        {
+            this.Dict = dict;
+            this.Monad = monad;
+        }
+        public IMonad<T> Monad { get; }
+
+        public T Value => Monad.Lift(Dict);
+
+        public IReadOnlyDictionary<string, object> Dict { get; }
+
+        public virtual IContext<T> OverrideName(string name, T br)
+            => new OverrideContext<T>(
+                this,
+                ImmutableDictionary<string, T>.Empty.SetItem(name, br));
+
+        public bool TryGetAccessor(string propName, out IValueAccessor<T> accessor)
+        {
+            accessor = new DictionaryAccessor<T>(propName);
+            return true;
+        }
+
+        public bool TryGetContext(object o, out IContext<T> context)
+        {
+            if (o is IReadOnlyDictionary<string, object> dic)
+            {
+                context = new DictionaryContext<T>(Monad, dic);
+                return true;
+            }
+
+            context = new ObjectContext<T>(Monad, o);
+            return true;
+        }
+    }
+}
