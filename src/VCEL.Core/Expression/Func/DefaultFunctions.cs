b6a0deb9b71fa49a8a19b8db2c8a16984918500f
Aaron Bloom <contact@aaronbloom.uk>
2021-06-15 17:44:59 +0800

    Add more maths functions

--- src/VCEL.Core/Expression/Func/DefaultFunctions.cs
+++ src/VCEL.Core/Expression/Func/DefaultFunctions.cs
@@ -11,11 +11,21 @@ namespace VCEL.Core.Expression.Func
 
         public DefaultFunctions()
         {
-            Register(
-                "abs",
-                args => args[0] == null
-                    ? null
-                    : Convert.ChangeType(Math.Abs(Convert.ToDouble(args[0])), args[0].GetType()));
+            RegisterEnsureOneArg("abs", arg => Math.Abs(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("acos", arg => Math.Acos(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("asin", arg => Math.Asin(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("atan", arg => Math.Atan(Convert.ToDouble(arg)));
+            RegisterEnsureTwoArgs("atan2", (arg1, arg2) => Math.Atan2(Convert.ToDouble(arg1), Convert.ToDouble(arg2)));
+            RegisterEnsureOneArg("ceiling", arg => Math.Ceiling(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("cos", arg => Math.Cos(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("cosh", arg => Math.Cosh(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("exp", arg => Math.Exp(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("floor", arg => Math.Floor(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("log", arg => Math.Log(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("log10", arg => Math.Log10(Convert.ToDouble(arg)));
+            Register("max", Enumerable.Max);
+            Register("min", Enumerable.Min);
+            RegisterEnsureTwoArgs("pow", (arg1, arg2) => Math.Pow(Convert.ToDouble(arg1), Convert.ToDouble(arg2)));
             Register(
                 "round",
                 args =>
@@ -31,13 +41,14 @@ namespace VCEL.Core.Expression.Func
 
                     return (object)null;
                 });
-            Register(
-                "sqrt",
-                args => args.Length == 1 && args[0] != null
-                    ? Math.Sqrt(Convert.ToDouble(args[0]))
-                    : (object)null);
-            Register("min", Enumerable.Min);
-            Register("max", Enumerable.Max);
+            RegisterEnsureOneArg("sign", arg => Math.Sign(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("sin", arg => Math.Sin(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("sinh", arg => Math.Sinh(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("sqrt", arg => Math.Sqrt(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("tan", arg => Math.Tan(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("tanh", arg => Math.Tanh(Convert.ToDouble(arg)));
+            RegisterEnsureOneArg("truncate", arg => Math.Truncate(Convert.ToDouble(arg)));
+
             Register("now", _ => DateTime.Now, TemporalDependency.Now);
             Register("today", _ => DateTime.Today, TemporalDependency.Today);
         }
@@ -53,11 +64,17 @@ namespace VCEL.Core.Expression.Func
         public void Register(string name, Func<object[], object> func)
             => this.Register(name, (args, _) => func(args), new FuncDependency(name));
 
+        public void RegisterEnsureOneArg(string name, Func<object, object> func)
+            => this.Register(name, (args, _) => args.Length == 1 && args[0] == null ? null : func(args[0]), new FuncDependency(name));
+
+        public void RegisterEnsureTwoArgs(string name, Func<object, object, object> func)
+            => this.Register(name, (args, _) => args.Length == 2 && args[0] == null || args[1] == null ? null : func(args[0], args[1]),
+                new FuncDependency(name));
+
         protected void Register(string name, Func<object[], IContext<T>, object> func, params IDependency[] deps)
             => functions[name.ToLower()] = new Function<T>(func, deps);
 
         protected void Register(string name, Func<object[], object> func, params IDependency[] deps)
             => functions[name.ToLower()] = new Function<T>((args, _) => func(args), deps);
     }
-
 }
