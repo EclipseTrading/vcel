e88dc3161ac27b80ccff3c6bcc09774bd253ee37
Trevor Leung <tleung@eclipseoptions.com>
2021-07-28 13:26:02 +0800

    VC-5125 optimize expression

--- src/VCEL.JS/Expression/ToJsValueOfOp.cs
+++ src/VCEL.JS/Expression/ToJsValueOfOp.cs
@@ -29,7 +29,7 @@ namespace VCEL.JS.Expression
         {
             var value = expression.Evaluate(context);
             return expression is ToJsPropertyOp 
-                ? $"({value} === null ? null : {value}.valueOf())" 
+                ? $"({value}?.valueOf() ?? null)" 
                 : value;
         }
     }
