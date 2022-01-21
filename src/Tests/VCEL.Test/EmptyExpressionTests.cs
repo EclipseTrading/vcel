d6354c8a53f5a76415a530332a3a7df523dac69b
Kevin Wong <kwong@eclipseoptions.com>
2022-01-21 14:14:55 +0800

    VC-6515 Update empty expression test cases

--- src/Tests/VCEL.Test/EmptyExpressionTests.cs
+++ src/Tests/VCEL.Test/EmptyExpressionTests.cs
@@ -1,17 +1,38 @@
 using NUnit.Framework;
+using VCEL.Core.Lang;
 using VCEL.Test.Shared;
 
 namespace VCEL.Test
 {
     public class EmptyExpressionTest
     {
-        [TestCase("", null)]
-        public void TestEmptyExpr(string exprString, object expected)
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(" \n ")]
+        public void TestEmptyExpr(string exprString)
         {
+            var maybeExpr = VCExpression.ParseMaybe(exprString);
+            var maybeResult = maybeExpr.Expression.Evaluate(new {});
+            Assert.IsFalse(maybeResult.HasValue);
             foreach (var expr in CompositeExpression.ParseMultiple(exprString))
             {
                 var result = expr.Expression.Evaluate(new { });
-                Assert.That(result, Is.EqualTo(expected));
+                Assert.IsNull(result);
+            }
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(" \n ")]
+        public void TestEmptyExprWithContext(string exprString)
+        {
+            var maybeExpr = VCExpression.ParseMaybe(exprString);
+            var maybeResult = maybeExpr.Expression.Evaluate(new { a = 10, b = 5});
+            Assert.IsFalse(maybeResult.HasValue);
+            foreach (var expr in CompositeExpression.ParseMultiple(exprString))
+            {
+                var result = expr.Expression.Evaluate(new { a = 10, b = 5});
+                Assert.IsNull(result);
             }
         }
     }
