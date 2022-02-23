964a28376dcc0b1424b2d87fb117408c99191508
Aaron Bloom <contact@aaronbloom.uk>
2022-02-23 11:11:33 +0800

    Use checked equals on the In operation items

--- src/VCEL.CSharp/Expression/ToCSharpInOp.cs
+++ src/VCEL.CSharp/Expression/ToCSharpInOp.cs
@@ -15,7 +15,7 @@ namespace VCEL.CSharp.Expression
 
         public override string Evaluate(object lv, object rv)
         {
-            return $@"CSharpHelper.IsNumber({lv}) ? {rv}.Any(elem => CSharpHelper.IsNumber(elem) ? Convert.ToDouble(elem) == Convert.ToDouble({lv}) : false) : {rv}.Contains(({lv}))";
+            return $@"{rv}.Any(elem => TypeOperation.EqualsChecked({lv}, elem))";
         }
     }
 }
