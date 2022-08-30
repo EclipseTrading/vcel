98b2e8914bb4be2f1b59383fd5a80153fe6eabcb
Andy Lai <alai@eclipseoptions.com>
2022-08-30 15:02:58 +0800

    VC-7537 use Math.floor and Number for int and long type conversion for JS instead

--- src/VCEL.JS/Expression/ToJsFunction.cs
+++ src/VCEL.JS/Expression/ToJsFunction.cs
@@ -32,8 +32,6 @@ namespace VCEL.JS.Expression
                  { "tan" , "Math.tan" },
                  { "tanh" , "Math.tanh" },
                  { "truncate" , "Math.trunc" },
-                 { "int", "parseInt" },
-                 { "long", "parseInt" },
                  { "double", "Number" },
                  { "decimal", "Number" },
                  { "string", "String" },
@@ -43,6 +41,13 @@ namespace VCEL.JS.Expression
                  { "datetime", "new Date" },
                  { "date", "new Date" }
              };
+
+        private static Dictionary<string, string> JSDoubleFunctionMap 
+            = new Dictionary<string, string> () 
+            {
+                { "int", "Math.floor(Number" },
+                { "long", "Math.floor(Number" },
+            };
 
         private static Dictionary<string, string> JSMethodMap
             = new Dictionary<string, string>()
@@ -92,6 +97,12 @@ namespace VCEL.JS.Expression
                     ? $"({jsFunc}({string.Join(",", args.Select(s => s.Evaluate(context)))}))"
                     : WarpVariableForNullChecking(context.Value, jsFunc, string.Join(",", args.Select(s => s.Evaluate(context))));
             }
+            if (JSDoubleFunctionMap.TryGetValue(name, out var jsDoubleFunc))
+            {
+                return IsContextEmpty(context)
+                    ? $"({jsDoubleFunc}({string.Join(",", args.Select(s => s.Evaluate(context)))})))"
+                    : WarpVariableForNullCheckingDoubleFunc(context.Value, jsDoubleFunc, string.Join(",", args.Select(s => s.Evaluate(context))));
+            }
             return $"({name}({string.Join(",", args.Select(s => s.Evaluate(context)))}))";
         }
 
@@ -104,6 +115,12 @@ namespace VCEL.JS.Expression
         {
             var defaultReturnVal = GetDefaultVal(func);
             return $"({variable} ? {variable}.{func}({args}) : {defaultReturnVal})";
+        }
+
+        private string WarpVariableForNullCheckingDoubleFunc(string variable, string func, string args)
+        {
+            var defaultReturnVal = GetDefaultVal(func);
+            return $"({variable} ? {variable}.{func}({args})) : {defaultReturnVal})";
         }
 
         private string GetDefaultVal(string func)
